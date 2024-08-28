98081bce9a94ec393d6d86c5c96c82415b4b2845
Patrick Dugan <patrick.buchanan.dugan@gmail.com>
2024-08-28 14:02:55 -0400

    Modified trading tickers

--- src/services/markets/manager.class.js
+++ src/services/markets/manager.class.js
@@ -8,10 +8,16 @@ var MarketsManager = (function () {
     }
     MarketsManager.prototype.getAvailableSpotMarkets = function () {
         try {
-            var ltcToken = (0, market_factory_1.createToken)('TEST', 'TEST Token 2', 4);
-            var testToken = (0, market_factory_1.createToken)('TEST2', 'TEST Token', 5);
+            var LTC = (0, market_factory_1.createToken)('LTC', 'LTC', 0);
+            var TBILL = (0, market_factory_1.createToken)('TBILL', 'TBILL', 5);
+            var TL = (0, market_factory_1.createToken)('TL':'TL',1)
+            var sLTC = (0, market_factory_1.createToken)('sLTC':'sLTC','s-1-5')
             var ltcMartkets = [
-                (0, market_factory_1.createMarket)(testToken, ltcToken, false),
+                (0, market_factory_1.createMarket)(TBILL, LTC, false),
+                (0, market_factory_1.createMarket)(TL, LTC, false),
+                (0, market_factory_1.createMarket)(sLTC, LTC, false),
+                (0, market_factory_1.createMarket)(TL, TBILL, false),
+                (0, market_factory_1.createMarket)(sLTC, TBILL, false)
             ];
             var ltcMarketType = (0, market_factory_1.createMarketType)('LTC', ltcMartkets, market_factory_1.MARKET_ICONS.LTC, false);
             var result = [ltcMarketType];
@@ -22,19 +28,22 @@ var MarketsManager = (function () {
         }
     };
     MarketsManager.prototype.getAvailableFuturesMarkets = function () {
+            var LTC = (0, market_factory_1.createToken)('LTC', 'Litecoin', 0);
+            var TBILL = (0, market_factory_1.createToken)('TBILL', 'US Treasury Bill', 5);
+            var TL = (0, market_factory_1.createToken)('TL':'TradeLayer Native Metacoin',1)
+            var BTCoracle = (0, market_factory_1.createToken)('BTC':'Bitcoin Oracle',1)
         try {
-            var wEthToken = (0, market_factory_1.createToken)('WETH', 'Wrapped ETH', 4);
-            var wBtcToken = (0, market_factory_1.createToken)('WBTC', 'Wrapped BTC', 8);
-            var usdToken = (0, market_factory_1.createToken)('USD', 'US Dollar', -3);
-            var adaToken = (0, market_factory_1.createToken)('WADA', 'Cardano', 18);
             var usdContracts = [
-                (0, market_factory_1.createContract)(adaToken, usdToken, 17, 'wADA/USD', adaToken, false),
-                (0, market_factory_1.createContract)(wEthToken, usdToken, 5, 'wETH/USD', wEthToken, false),
-                (0, market_factory_1.createContract)(wBtcToken, usdToken, 7, 'wBTC/USD', wBtcToken, true),
+                (0, market_factory_1.createContract)(TL, TBILL, 4, 'TL/TBILL', TBILL, false),
+                (0, market_factory_1.createContract)(LTC, TBILL, 5, 'LTC/TBILL', TBILL, false),
+                (0, market_factory_1.createContract)(BTCoracle, TBILL, 2, 'BTC/USD', TBILL, false)
             ];
-            var dogeContracts = [];
-            var ltcMarketType = (0, market_factory_1.createMarketType)('USD', usdContracts, market_factory_1.MARKET_ICONS.USD, false);
-            var dogeMarketType = (0, market_factory_1.createMarketType)('DOGE', dogeContracts, market_factory_1.MARKET_ICONS.DOGE, true);
+            var ltcContracts = [
+                (0, market_factory_1.createContract)(TL, LTC, 1, 'TL/LTC', TL, false),
+            ]
+            //var dogeContracts = [];
+            var ltcMarketType = (0, market_factory_1.createMarketType)('LTC', ltcContracts, market_factory_1.MARKET_ICONS.LTC, false);
+            var usdMarketType = (0, market_factory_1.createMarketType)('USD', usdContracts, market_factory_1.MARKET_ICONS.USD, true);
             var result = [ltcMarketType, dogeMarketType];
             return { data: result };
         }
