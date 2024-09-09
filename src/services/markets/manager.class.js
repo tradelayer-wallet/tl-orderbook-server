87f9979209f1700ef001dc9bf6c632155054466c
Patrick Dugan <patrick.buchanan.dugan@gmail.com>
2024-09-09 15:42:54 -0300

    Fix stuff from merge

--- src/services/markets/manager.class.js
+++ src/services/markets/manager.class.js
@@ -1,56 +1,58 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MarketsManager = void 0;
-var market_factory_1 = require("./market.factory");
-var MarketsManager = (function () {
-    function MarketsManager() {
-        console.log("Markets Service Initialized");
+import { IContract, IMarket, IMarketType } from "../../utils/types/markets.types";
+import { IResult } from "../../utils/types/mix.types";
+import { createContract, createMarket, createMarketType, createToken, MARKET_ICONS } from "./market.factory";
+
+export class MarketsManager {
+    constructor() {
+        console.log(`Markets Service Initialized`);
     }
-    MarketsManager.prototype.getAvailableSpotMarkets = function () {
+
+    getAvailableSpotMarkets(): IResult {
         try {
-            var LTC = (0, market_factory_1.createToken)('LTC', 'LTC', 0);
-            var TBILL = (0, market_factory_1.createToken)('TBILL', 'TBILL', 5);
-            var TL = (0, market_factory_1.createToken)('TL','TL',1)
-            var sLTC = (0, market_factory_1.createToken)('sLTC','sLTC','s-1-5')
-            var ltcMarkets = [
-                (0, market_factory_1.createMarket)(LTC, TBILL, false),
-                (0, market_factory_1.createMarket)(TL, LTC, false),
-                (0, market_factory_1.createMarket)(sLTC, LTC, false),
-                (0, market_factory_1.createMarket)(TL, TBILL, false),
-                (0, market_factory_1.createMarket)(sLTC, TBILL, false)
+            const LTC = createToken('LTC', 'LTC', 0);
+            const TBILL = createToken('TBILL', 'TBILL', 5);
+            const TL = createToken('TL', 'TL', 1);
+            const sLTC = createToken('sLTC', 'sLTC', 's-1-5');
+
+            const ltcMarkets: IMarket[] = [
+                createMarket(LTC, TBILL, false),
+                createMarket(TL, LTC, false),
+                createMarket(sLTC, LTC, false),
+                createMarket(TL, TBILL, false),
+                createMarket(sLTC, TBILL, false)
             ];
-            var ltcMarketType = (0, market_factory_1.createMarketType)('LTC', ltcMarkets, market_factory_1.MARKET_ICONS.LTC, false);
-            var result = [ltcMarketType];
+
+            const ltcMarketType = createMarketType('LTC', ltcMarkets, MARKET_ICONS.LTC, false);
+            const result: IMarketType[] = [ltcMarketType];
             return { data: result };
-        }
-        catch (error) {
+        } catch (error) {
             return { error: error.message };
         }
-    };
-    MarketsManager.prototype.getAvailableFuturesMarkets = function () {
-            var LTC = (0, market_factory_1.createToken)('LTC', 'Litecoin', 0);
-            var TBILL = (0, market_factory_1.createToken)('TBILL', 'US Treasury Bill', 5);
-            var TL = (0, market_factory_1.createToken)('TL','TradeLayer Native Metacoin',1)
-            var BTCoracle = (0, market_factory_1.createToken)('BTC','Bitcoin Oracle',1)
+    }
+
+    getAvailableFuturesMarkets(): IResult {
         try {
-            var usdContracts = [
-                (0, market_factory_1.createContract)(TL, TBILL, 4, 'TL/TBILL', TBILL, false),
-                (0, market_factory_1.createContract)(LTC, TBILL, 5, 'LTC/TBILL', TBILL, false),
-                (0, market_factory_1.createContract)(BTCoracle, TBILL, 2, 'BTC/USD', TBILL, false)
+            const LTC = createToken('LTC', 'Litecoin', 0);
+            const TBILL = createToken('TBILL', 'US Treasury Bill', 5);
+            const TL = createToken('TL', 'TradeLayer Native Metacoin', 1);
+            const BTCoracle = createToken('BTC', 'Bitcoin Oracle', 1);
+
+            const usdContracts: IContract[] = [
+                createContract(TL, TBILL, 4, 'TL/TBILL', TBILL, false),
+                createContract(LTC, TBILL, 5, 'LTC/TBILL', TBILL, false),
+                createContract(BTCoracle, TBILL, 2, 'BTC/USD', TBILL, false)
             ];
-            var ltcContracts = [
-                (0, market_factory_1.createContract)(TL, LTC, 1, 'TL/LTC', TL, false),
-            ]
-            //var dogeContracts = [];
-            var ltcMarketType = (0, market_factory_1.createMarketType)('LTC', ltcContracts, market_factory_1.MARKET_ICONS.LTC, false);
-            var usdMarketType = (0, market_factory_1.createMarketType)('USD', usdContracts, market_factory_1.MARKET_ICONS.USD, true);
-            var result = [ltcMarketType, dogeMarketType];
+
+            const ltcContracts: IContract[] = [
+                createContract(TL, LTC, 1, 'TL/LTC', TL, false),
+            ];
+
+            const ltcMarketType = createMarketType('LTC', ltcContracts, MARKET_ICONS.LTC, false);
+            const usdMarketType = createMarketType('USD', usdContracts, MARKET_ICONS.USD, true);
+            const result: IMarketType[] = [ltcMarketType, usdMarketType];
             return { data: result };
-        }
-        catch (error) {
+        } catch (error) {
             return { error: error.message };
         }
-    };
-    return MarketsManager;
-}());
-exports.MarketsManager = MarketsManager;
+    }
+}
