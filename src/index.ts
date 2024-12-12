ac50a7df0cee9879c45730d7a9a3382d4299a2ea
Patrick Dugan <patrick.buchanan.dugan@gmail.com>
2024-12-12 13:25:01 -0300

    fast

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,57 @@
-import Fastify, { FastifyServerOptions } from 'fastify';
+import * as fs from 'fs'; // Import fs for reading SSL certificates
+import Fastify from 'fastify';
 import { handleRoutes } from './routes/routes';
 import { envConfig } from './config/env.config';
 import { initSocketService } from './services/socket';
 import { initOrderbookService } from './services/orderbook';
 import { initMarketsService } from './services/markets';
-import * as fs from 'fs'
 
 const SERVER_PORT = envConfig.SERVER_PORT || 443;
-const HTTP_PORT = envConfig.HTTP_PORT || 9191;
-const SECURE_OPTIONS: FastifyServerOptions = {
-    https: {
-        key: fs.readFileSync('/home/ubuntu/ssl/privkey.pem'),
-        cert: fs.readFileSync('/home/ubuntu/ssl/fullchain.pem'),
-    },
+const HTTP_PORT = envConfig.HTTP_PORT || 80;
+
+// Load SSL certificates
+const SECURE_OPTIONS = {
+    key: fs.readFileSync('/home/ubuntu/ssl/privkey.pem'),
+    cert: fs.readFileSync('/home/ubuntu/ssl/fullchain.pem'),
 };
-// Create servers
-const server = Fastify(SECURE_OPTIONS); // Secure server (HTTPS)
-const serverHTTP = Fastify(); // Non-secure server (HTTP)
 
-// Initialize services
+// Create HTTPS Fastify instance
+const server = Fastify({
+    logger: true,
+    https: SECURE_OPTIONS, // Attach HTTPS options here
+});
+
+// Create HTTP Fastify instance
+const serverHTTP = Fastify({ logger: true });
+
+// Initialize routes and services
 handleRoutes(server);
-handleRoutes(serverHTTP);
+initSocketService(server);
 
-initSocketService(server); // Attach WebSocket to secure server
-initSocketService(serverHTTP); // Attach WebSocket to non-secure server
+handleRoutes(serverHTTP);
+initSocketService(serverHTTP);
 
 initOrderbookService();
 initMarketsService();
 
-// Start the secure server (HTTPS)
+// Start HTTPS server
 server
     .listen(SERVER_PORT, '0.0.0.0')
     .then(() => {
-        console.log(`Secure Server Started: https://localhost:${SERVER_PORT}`);
+        console.log(`Secure server running on https://localhost:${SERVER_PORT}`);
     })
-    .catch((error) => {
-        console.error('Error starting secure server:', error.message);
+    .catch((err) => {
+        console.error('Error starting HTTPS server:', err.message);
         process.exit(1);
     });
 
-// Start the non-secure server (HTTP)
+// Start HTTP server
 serverHTTP
     .listen(HTTP_PORT, '0.0.0.0')
     .then(() => {
-        console.log(`Non-Secure Server Started: http://localhost:${HTTP_PORT}`);
+        console.log(`Non-secure server running on http://localhost:${HTTP_PORT}`);
     })
-    .catch((error) => {
-        console.error('Error starting non-secure server:', error.message);
+    .catch((err) => {
+        console.error('Error starting HTTP server:', err.message);
         process.exit(1);
     });
