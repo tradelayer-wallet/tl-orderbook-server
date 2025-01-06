c3658aa834f84ca765329fcb12dc137fcf2164ec
Patrick Dugan <patrick.buchanan.dugan@gmail.com>
2025-01-06 19:14:26 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import * as fs from 'fs'; // Import fs for reading SSL certificates
 import Fastify from 'fastify';
 import { handleRoutes } from './routes/routes';
 import { envConfig } from './config/env.config';
@@ -6,50 +5,22 @@ import { initSocketService } from './services/socket';
 import { initOrderbookService } from './services/orderbook';
 import { initMarketsService } from './services/markets';
 
-const SERVER_PORT = envConfig.SERVER_PORT || 443;
-const HTTP_PORT = envConfig.HTTP_PORT || 9191;
+const SERVER_PORT = envConfig.HTTP_PORT || 9191; // Default HTTP port
 
-// Load SSL certificates
-const SECURE_OPTIONS = {
-    key: fs.readFileSync('/home/ubuntu/ssl/privkey.pem'),
-    cert: fs.readFileSync('/home/ubuntu/ssl/fullchain.pem'),
-};
-
-// Create HTTPS Fastify instance
-const server = Fastify({
-    logger: true,
-    https: SECURE_OPTIONS, // Attach HTTPS options here
-});
-
-// Create HTTP Fastify instance
-const serverHTTP = Fastify({ logger: true });
+// Create Fastify HTTP server
+const server = Fastify({ logger: true });
 
 // Initialize routes and services
 handleRoutes(server);
 initSocketService(server);
-
-handleRoutes(serverHTTP);
-initSocketService(serverHTTP);
-
 initOrderbookService();
 initMarketsService();
 
-// Start HTTPS server
+// Start HTTP server
 server
     .listen(SERVER_PORT, '0.0.0.0')
     .then(() => {
-        console.log(`Secure server running on https://localhost:${SERVER_PORT}`);
-    })
-    .catch((err) => {
-        console.error('Error starting HTTPS server:', err.message);
-        process.exit(1);
-    });
-
-// Start HTTP server
-serverHTTP
-    .listen(HTTP_PORT, '0.0.0.0')
-    .then(() => {
-        console.log(`Non-secure server running on http://localhost:${HTTP_PORT}`);
+        console.log(`Server running on http://localhost:${SERVER_PORT}`);
     })
     .catch((err) => {
         console.error('Error starting HTTP server:', err.message);
