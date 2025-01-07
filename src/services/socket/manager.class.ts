142671e362e4985b121e0c6d333e4e3777c7af31
Patrick Dugan <patrick.buchanan.dugan@gmail.com>
2025-01-07 19:17:22 -0300

    Update manager.class.ts

--- src/services/socket/manager.class.ts
+++ src/services/socket/manager.class.ts
@@ -101,6 +101,7 @@ const onNewOrder = (socket: Socket) => async (rawOrder: TRawOrder) => {
     }
 
     if (res.data.order) {
+        console.log('inside the if res.data.order block '+socket)
         socket.emit(OrderEmitEvents.SAVED, res.data.order.uuid);
         const openedOrders = orderbookManager.getOrdersBySocketId(socket.id);
         const orderHistory = orderbookManager.getOrdersHistory();
