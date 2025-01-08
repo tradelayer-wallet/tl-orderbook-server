9b4ea96320e8c25e8e9097b5510a67d179093c60
Patrick Dugan <patrick.buchanan.dugan@gmail.com>
2025-01-08 13:56:55 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,9 @@ import { handleRoutes } from './routes/routes';
 import { envConfig } from './config/env.config';
 import { initOrderbookService } from './services/orderbook';
 import { initMarketsService } from './services/markets';
-import { SocketManager } from './services/socket/manager.class';
+//import { SocketManager } from './services/socket/manager.class';
+import { initSocketService } from './services/socket';
+
 
 const HTTPS_PORT = 443;
 const HTTP_PORT = envConfig.HTTP_PORT || 9191;
@@ -39,15 +41,15 @@ Promise.all([
   serverHTTPS.listen({ port: HTTPS_PORT, host: '0.0.0.0' }),
   serverHTTP.listen({ port: HTTP_PORT, host: '0.0.0.0' }),
 ])
-  .then(async ([httpsAddress, httpAddress]) => {
-    const httpsPort = typeof httpsAddress === 'string'
-    const httpPort = typeof httpAddress === 'string'
-
-    console.log(`Secure server running at port: ${httpsPort}`);
-    console.log(`Non-secure server running at port: ${httpPort}`);
-
-    await SocketManager.init([serverHTTPS, serverHTTP]);
+  .then(async () => {
+    await initSocketService([serverHTTPS, serverHTTP]);
+    console.log('All services initialized successfully.');
   })
+  .catch((err) => {
+    console.error('Error initializing services:', err.message);
+    process.exit(1);
+  });
+
   .catch((err) => {
     console.error('Error starting servers:', err.message);
     process.exit(1);
