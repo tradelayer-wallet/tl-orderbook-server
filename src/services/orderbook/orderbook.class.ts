be07c14bd0411a65a4d12d783778f5105d4fa047
Patrick Dugan <patrick.buchanan.dugan@gmail.com>
2025-01-08 18:43:04 -0300

    Update orderbook.class.ts

--- src/services/orderbook/orderbook.class.ts
+++ src/services/orderbook/orderbook.class.ts
@@ -112,7 +112,7 @@ export class Orderbook {
             const sellerSocketId = tradeInfo.seller.socketId;
             const buyerSocket = SocketManager.io.sockets.sockets.get(buyerSocketId);
             const sellerSocket = SocketManager.io.sockets.sockets.get(sellerSocketId);
-            console.log('inside new channel '+buyerSocketId+' '+sellerSocketId+' '+buyerSocket+' '+sellerSocket)
+            console.log('inside new channel '+buyerSocketId+' '+sellerSocketId+' '+buyerSocket?.id+' '+sellerSocket?.id)
             const channel = new ChannelSwap(buyerSocket, sellerSocket, tradeInfo, unfilled);
             const channelRes = await channel.onReady();
             console.log('channelRes '+JSON.stringify(channelRes))
