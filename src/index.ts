c580a5fcf995e2f433b0a9c8d89f87e5c32a608e
Patrick Dugan <patrick.buchanan.dugan@gmail.com>
2025-07-10 15:57:02 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,12 @@ const server = new HyperExpress.Server();
 
 // Initialize services with the server instance
 handleRoutes(server);
-initSocketService(server);
+// Register a WebSocket route for clients (orderbook desktop/web wallets, etc)
+server.ws('/ws', (ws) => {
+    // Pass the ws connection to your socket service
+    // This is where message/event routing happens
+    initSocketService(ws);
+});
 initOrderbookService();
 initMarketsService();
 
