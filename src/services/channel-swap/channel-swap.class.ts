ad9ab4aed0c47aab6a2da385922355e59a5bc3cd
Patrick Dugan <patrick.buchanan.dugan@gmail.com>
2025-07-13 20:44:35 -0400

    trying update to channel swap mgmr

--- src/services/channel-swap/channel-swap.class.ts
+++ src/services/channel-swap/channel-swap.class.ts
@@ -1,103 +1,104 @@
 // channel-swap.class.ts
-import { SocketEventManager } from './socket-event-manager';
-import { IResultChannelSwap } from "../../utils/types/mix.types";
-import { ITradeInfo, TOrder } from "../../utils/types/orderbook.types";
 import { Websocket } from 'hyper-express';
+import { SocketEventManager, EventHandler } from './socket-event-manager';
+import { IResultChannelSwap } from '../../utils/types/mix.types';
+import { ITradeInfo, TOrder } from '../../utils/types/orderbook.types';
 
-class SwapEvent {
-    constructor(
-        public eventName: string,
-        public socketId: string,
-        public data: any,
-    ) {}
+export class SwapEvent {
+  constructor(
+    public eventName: string,
+    public socketId: string,
+    public data: any
+  ) {}
 }
 
 const swapEventName = 'swap';
 
 export class ChannelSwap {
-    private readyRes: (value: IResultChannelSwap) => void;
-    private clientManager: SocketEventManager;
-    private dealerManager: SocketEventManager;
-    private isClosed: boolean = false;
+  private ready!: (v: IResultChannelSwap) => void;
+  private readonly buyerId: string;
+  private readonly clientMgr: SocketEventManager;
+  private readonly dealerMgr: SocketEventManager;
+  private closed = false;
 
-    constructor(
-        private client: Websocket, 
-        private dealer: Websocket, 
-        private tradeInfo: ITradeInfo, 
-        private unfilled: TOrder,
-    ) {
-        this.clientManager = new SocketEventManager(client);
-        this.dealerManager = new SocketEventManager(dealer);
-        this.onReady();
-        this.openChannel();
-    }
+  constructor(
+    private client: Websocket,
+    private dealer: Websocket,
+    private tradeInfo: ITradeInfo,
+    private unfilled: TOrder
+  ) {
+    // Wrap each HyperExpress socket with our event manager
+    this.clientMgr = new SocketEventManager(client);
+    this.dealerMgr = new SocketEventManager(dealer);
+    this.buyerId = tradeInfo.buyer.socketId;
 
-    onReady(): Promise<IResultChannelSwap> {
-        return new Promise<IResultChannelSwap>((res) => {
-            this.readyRes = res;
-        });
-    }
+    this.openChannel();
+  }
 
-    private openChannel(): void {
-        this.handleEvents();
-        const buyerSocketId = this.tradeInfo.buyer.socketId;
-        const trade = { tradeInfo: this.tradeInfo, unfilled: this.unfilled };
-        const isBuyerClient = (this.client as any).id === buyerSocketId;
+  /** resolves when BUYER:STEP6 or TERMINATE_TRADE received */
+  onReady(): Promise<IResultChannelSwap> {
+    return new Promise<IResultChannelSwap>(res => (this.ready = res));
+  }
 
-        // Send 'new-channel' event to both client and dealer
-        this.clientManager.send('new-channel', { ...trade, isBuyer: isBuyerClient });
-        this.dealerManager.send('new-channel', { ...trade, isBuyer: !isBuyerClient });
-    }
+  /* ------------------------------------------------------------------ */
+  /*                          Private helpers                           */
+  /* ------------------------------------------------------------------ */
 
-    private handleEvents(): void {
-        this.handleEventsAndPassToCounterparty();
+  private openChannel() {
+    this.pipeSwapEvents();   // bidirectional relay
+    this.monitorTerminalEvents();
 
-        const onSwapEvent = (swapEvent: SwapEvent) => {
-            const { eventName, socketId, data } = swapEvent;
-            console.log('new swap event ' +JSON.stringify(swapEvent))
-            if (eventName === "BUYER:STEP6") {
-                if (this.readyRes) this.readyRes({ data: { txid: data } });
-                this.closeChannel();
-            }
+    const trade = { tradeInfo: this.tradeInfo, unfilled: this.unfilled };
 
-            if (eventName === "TERMINATE_TRADE") {
-                if (this.readyRes) this.readyRes({ error: data, socketId });
-                this.closeChannel();
-            }
-        };
+    // Send 'new-channel' to each side
+    this.clientMgr.emit('new-channel', {
+      ...trade,
+      isBuyer: this.client.id === this.buyerId
+    });
+    this.dealerMgr.emit('new-channel', {
+      ...trade,
+      isBuyer: this.dealer.id === this.buyerId
+    });
+  }
 
-        this.clientManager.on(swapEventName, onSwapEvent);
-        this.dealerManager.on(swapEventName, onSwapEvent);
-    }
+  /** Relay `socketId::swap` messages between the two peers (exactly like legacy) */
+  private pipeSwapEvents() {
+    const clientSwapEvt = `${this.client.id}::${swapEventName}`;
+    const dealerSwapEvt = `${this.dealer.id}::${swapEventName}`;
 
-    private handleEventsAndPassToCounterparty() {
-        const clientId = (this.client as any).id;
-        const dealerId = (this.dealer as any).id;
+    this.clientMgr.on(clientSwapEvt, data =>
+      this.dealerMgr.emit(clientSwapEvt, data)
+    );
+    this.dealerMgr.on(dealerSwapEvt, data =>
+      this.clientMgr.emit(dealerSwapEvt, data)
+    );
+  }
 
-        const passToDealer = (data: any) => {
-            this.dealerManager.send(`${clientId}::${swapEventName}`, data);
-        };
+  /** Watch for BUYER:STEP6 or TERMINATE_TRADE and resolve/close */
+  private monitorTerminalEvents() {
+    const handler: EventHandler = (swap: SwapEvent) => {
+      const { eventName, socketId, data } = swap;
 
-        const passToClient = (data: any) => {
-            this.clientManager.send(`${dealerId}::${swapEventName}`, data);
-        };
-
-        this.clientManager.on(`${clientId}::${swapEventName}`, passToDealer);
-        this.dealerManager.on(`${dealerId}::${swapEventName}`, passToClient);
-    }
-
-    private closeChannel() {
-        this.isClosed = true;
-        // Remove event listeners
-        this.clientManager.off(swapEventName, this.handleSwapEvent);
-        this.dealerManager.off(swapEventName, this.handleSwapEvent);
-        // Close the event managers
-        this.clientManager.close();
-        this.dealerManager.close();
-    }
-
-    // Added this method to avoid closure issues with 'this'
-    private handleSwapEvent = (swapEvent: SwapEvent) => {
-        // Implementation as above in handleEvents
+      if (eventName === 'BUYER:STEP6') {
+        this.ready?.({ data: { txid: data } });
+        this.close();
+      }
+      if (eventName === 'TERMINATE_TRADE') {
+        this.ready?.({ error: data, socketId });
+        this.close();
+      }
     };
+
+    // Subscribe on both managers for any generic 'swap' broadcast
+    this.clientMgr.on(swapEventName, handler);
+    this.dealerMgr.on(swapEventName, handler);
+  }
+
+  /** Clean up listeners & managers */
+  private close() {
+    if (this.closed) return;
+    this.closed = true;
+    this.clientMgr.dispose();
+    this.dealerMgr.dispose();
+  }
 }
