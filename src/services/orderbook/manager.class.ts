e6715f867d52192cb41e3280a00f39eb7331ee83
Patrick Dugan <patrick.buchanan.dugan@gmail.com>
2025-07-14 10:52:23 -0400

    Update manager.class.ts

--- src/services/orderbook/manager.class.ts
+++ src/services/orderbook/manager.class.ts
@@ -24,6 +24,7 @@ export class OrderbookManager {
     }
 
     async addOrder(order: TOrder, noTrades: boolean = false): Promise<IResult<{ order?: TOrder, trade?: any }>> {
+        console.log('new order '+JSON.stringify(order)+' '+JSON.stringify(trade))
             try {
                 const existingOrderbook = this.orderbooks.find(b => b.checkCompatible(order));
                 const res = existingOrderbook
