bf3beaa7dacd45b51da049a509ce570eabe85497
Patrick Dugan <patrick.buchanan.dugan@gmail.com>
2025-08-01 09:38:55 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import { initSocketService } from './services/socket';
 import { initOrderbookService } from './services/orderbook';
 import { initMarketsService }   from './services/markets';
 
-const HTTP_PORT  = process.env.HTTP_PORT ? parseInt(process.env.HTTP_PORT) : 9191;
+const HTTP_PORT  = process.env.HTTP_PORT ? parseInt(process.env.HTTP_PORT) : 3001;
 const HTTPS_PORT = process.env.HTTPS_PORT ? parseInt(process.env.HTTPS_PORT) : 443;
 
 
